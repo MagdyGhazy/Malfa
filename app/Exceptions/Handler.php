ffbd3a12246e682e304802acf829e59e361f29fd
Magdy <129180170+MagdyGhazy@users.noreply.github.com>
2025-05-27 14:38:15 +0300

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,16 @@
 
 namespace App\Exceptions;
 
+use App\Http\Traits\ResponseTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ResponseTrait;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +31,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e): JsonResponse
+    {
+        if ($e instanceof ValidationException) {
+            return $this->error((object)[], 422, 'Validation failed', $e->errors());
+        }
+
+        return parent::render($request, $e);
     }
 }
